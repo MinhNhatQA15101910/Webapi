b9e2754b9b55ea0e731f8433057073a56b843c3c
Quan <doquan020903@gmail.com>
2025-06-03 02:11:14 +0700

    feat: update entity constraint for ProductSize

---
+++ Webapi.Infrastructure.Persistence/Migrations/20250602182054_UpdateProductSizeProductIdNullableEntity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Webapi.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductSizeProductIdNullableEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductSizes_Products_ProductId",
+                table: "ProductSizes");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "ProductSizes",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductSizes_Products_ProductId",
+                table: "ProductSizes",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductSizes_Products_ProductId",
+                table: "ProductSizes");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "ProductSizes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductSizes_Products_ProductId",
+                table: "ProductSizes",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
