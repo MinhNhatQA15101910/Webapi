9fb5294790fb5cf72e9b25f73da136fa710fc1b6
Quan <doquan020903@gmail.com>
2025-06-03 02:17:25 +0700

    feat: add update update category handler

---
+++ Webapi.Application/CategoryCQRS/Commands/UpdateCategory/UpdateCategoryHandler.cs
@@ -0,0 +1,36 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Webapi.Application.Common.Exceptions;
+using Webapi.Application.Common.Extensions;
+using Webapi.Application.Common.Interfaces.MediatR;
+using Webapi.Domain.Interfaces;
+using Webapi.SharedKernel.DTOs;
+
+namespace Webapi.Application.CategoryCQRS.Commands.UpdateCategory;
+
+public class UpdateCategoryHandler(
+    IHttpContextAccessor httpContextAccessor,
+    IUnitOfWork unitOfWork,
+    IMapper mapper
+) : ICommandHandler<UpdateCategoryCommand, CategoryDto>
+{
+    public async Task<CategoryDto> Handle(UpdateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var category = await unitOfWork.CategoryRepository.GetByIdAsync(request.Id, cancellationToken);
+        
+        if (category == null)
+        {
+            throw new Exception($"Category with ID {request.Id} not found");
+        }
+        
+        // Update category properties
+        category.Name = request.CategoryDto.Name;
+        category.Description = request.CategoryDto.Description;
+        category.Size = request.CategoryDto.Size;
+        category.Type = request.CategoryDto.Type;
+        category.UpdatedAt = DateTime.UtcNow;
+        
+        unitOfWork.CategoryRepository.Update(category);
+        return mapper.Map<CategoryDto>(category);
+    }
+}
