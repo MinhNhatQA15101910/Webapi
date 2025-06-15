ac7aae998e0bad39b267f5f28c123da2a02bca9a
Do Nhat <dmnhat.pt@gmail.com>
2025-06-15 17:45:55 +0700

    Creating migration.

---
+++ Webapi.Infrastructure.Persistence/Migrations/20250615104433_UpdateOrdersRelated.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Webapi.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOrdersRelated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_Products_ProductId",
+                table: "OrderProducts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductSizes_Products_ProductId",
+                table: "ProductSizes");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "OrderProducts",
+                newName: "ProductSizeId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProducts_ProductId",
+                table: "OrderProducts",
+                newName: "IX_OrderProducts_ProductSizeId");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "ProductSizes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_ProductSizes_ProductSizeId",
+                table: "OrderProducts",
+                column: "ProductSizeId",
+                principalTable: "ProductSizes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductSizes_Products_ProductId",
+                table: "ProductSizes",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderProducts_ProductSizes_ProductSizeId",
+                table: "OrderProducts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductSizes_Products_ProductId",
+                table: "ProductSizes");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductSizeId",
+                table: "OrderProducts",
+                newName: "ProductId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_OrderProducts_ProductSizeId",
+                table: "OrderProducts",
+                newName: "IX_OrderProducts_ProductId");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductId",
+                table: "ProductSizes",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "TEXT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderProducts_Products_ProductId",
+                table: "OrderProducts",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductSizes_Products_ProductId",
+                table: "ProductSizes",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "Id");
+        }
+    }
+}
