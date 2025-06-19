a10fda81c555db0104e350145ef27573313ff4be
Do Nhat <dmnhat.pt@gmail.com>
2025-06-19 16:18:36 +0700

    Adding get orders api.

--- Webapi.Presentation/Controllers/OrdersController.cs
+++ Webapi.Presentation/Controllers/OrdersController.cs
@@ -3,7 +3,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Webapi.Application.OrdersCQRS.Commands.CreateOrder;
 using Webapi.Application.OrdersCQRS.Queries.GetOrderById;
+using Webapi.Application.OrdersCQRS.Queries.GetOrders;
+using Webapi.Presentation.Extensions;
 using Webapi.SharedKernel.DTOs.Orders;
+using Webapi.SharedKernel.Params;
 
 namespace Webapi.Presentation.Controllers;
 
@@ -18,10 +21,20 @@ public class OrdersController(IMediator mediator) : ControllerBase
         return await mediator.Send(new GetOrderByIdQuery(orderId));
     }
 
+    [HttpGet]
+    public async Task<IEnumerable<OrderDto>> GetOrders([FromQuery] OrderParams orderParams)
+    {
+        var orders = await mediator.Send(new GetOrdersQuery(orderParams));
+
+        Response.AddPaginationHeader(orders);
+
+        return orders;
+    }
+
     [HttpPost]
     public async Task<ActionResult<OrderDto>> CreateOrder(CreateOrderDto createOrderDto)
     {
         var order = await mediator.Send(new CreateOrderCommand(createOrderDto));
-        return Ok(order);
+        return CreatedAtAction(nameof(GetOrderById), new { orderId = order.Id }, order);
     }
 }
--- Webapi.SharedKernel/DTOs/Orders/OrderDto.cs
+++ Webapi.SharedKernel/DTOs/Orders/OrderDto.cs
@@ -3,7 +3,8 @@ namespace Webapi.SharedKernel.DTOs.Orders;
 public class OrderDto
 {
     public Guid Id { get; set; }
-    public double TotalPrice { get; set; }
+    public Guid OwnerId { get; set; }
+    public decimal TotalPrice { get; set; }
     public string ShippingType { get; set; } = string.Empty;
     public double ShippingCost { get; set; }
     public string OrderState { get; set; } = string.Empty;
