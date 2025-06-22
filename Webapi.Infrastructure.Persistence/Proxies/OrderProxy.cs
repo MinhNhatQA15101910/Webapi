6319639a00aa33436e5c75b74d88b4ed8ec88cbc
Do Nhat <dmnhat.pt@gmail.com>
2025-06-22 18:37:10 +0700

    Updating proxy pattern for orders.

--- Webapi.Infrastructure.Persistence/Proxies/OrderProxy.cs
+++ Webapi.Infrastructure.Persistence/Proxies/OrderProxy.cs
@@ -21,34 +21,71 @@ public class OrderProxy(
 
     public async Task<Order?> GetOrderByIdAsync(Guid orderId, CancellationToken cancellationToken)
     {
-        return await orderRepository.GetOrderByIdAsync(orderId, cancellationToken);
-        
-        // var cacheKey = $"Orders_/{orderId}";
+        var cacheKey = $"Orders_/{orderId}";
 
-        // if (!cacheService.TryGetValue(cacheKey, out Order? order))
-        // {
-        //     // Fetch countries from the database.
-        //     order = await orderRepository.GetOrderByIdAsync(orderId, cancellationToken);
+        if (!cacheService.TryGetValue(cacheKey, out Order? order))
+        {
+            // Fetch countries from the database.
+            order = await orderRepository.GetOrderByIdAsync(orderId, cancellationToken);
 
-        //     cacheService.Set(cacheKey, order);
-        // }
+            cacheService.Set(cacheKey, order);
+        }
 
-        // return order;
+        return order;
     }
 
     public async Task<PagedList<OrderDto>> GetOrdersAsync(Guid? userId, OrderParams orderParams, CancellationToken cancellationToken = default)
     {
-        return await orderRepository.GetOrdersAsync(userId, orderParams, cancellationToken);
+        var cacheKey = GetOrdersQueryCacheKey(userId, orderParams);
+
+        if (!cacheService.TryGetValue(cacheKey, out PagedList<OrderDto>? orders))
+        {
+            // Fetch countries from the database.
+            orders = await orderRepository.GetOrdersAsync(userId, orderParams, cancellationToken);
+
+            cacheService.Set(cacheKey, orders);
+        }
+
+        return orders ?? new PagedList<OrderDto>([], 0, orderParams.PageNumber, orderParams.PageSize);
     }
 
     public void Update(Order order)
     {
         orderRepository.Update(order);
+
+        UpdateCacheForOrderUpdated(order);
+    }
+
+    private static string GetOrdersQueryCacheKey(Guid? currentUserId, OrderParams orderParams)
+    {
+        var cacheKey = $"Orders_";
+        cacheKey += $"?pageNumber={orderParams.PageNumber}";
+        cacheKey += $"&pageSize={orderParams.PageSize}";
+        if (currentUserId.HasValue)
+            cacheKey += $"&currentUserId={currentUserId.Value}";
+        if (orderParams.ShippingType is not null)
+            cacheKey += $"&shippingType={orderParams.ShippingType}";
+        if (orderParams.OrderState is not null)
+            cacheKey += $"&orderState={orderParams.OrderState}";
+        cacheKey += $"&minPrice={orderParams.MinPrice}";
+        cacheKey += $"&maxPrice={orderParams.MaxPrice}";
+        cacheKey += $"&orderBy={orderParams.OrderBy}";
+        cacheKey += $"&sortBy={orderParams.SortBy}";
+        return cacheKey;
     }
 
     private void UpdateCacheForOrderAdded()
     {
         var cacheKey = "Orders_?";
         cacheService.RemoveKeysStartingWith(cacheKey);
+    }
+
+    private void UpdateCacheForOrderUpdated(Order order)
+    {
+        var cacheKeyForList = "Orders_?";
+        cacheService.RemoveKeysStartingWith(cacheKeyForList);
+
+        var cacheKeyForSingle = $"Orders_/{order.Id}";
+        cacheService.Set(cacheKeyForSingle, order);
     }
 }
